1802f7febe089000ec81711485169e495cee7e6a
Jason <jason-git@outlook.com>
2025-03-04 15:16:23 +0800

    no message

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  editPage: false,
+  feedback: false,
+  footer: false,
 })
 
 module.exports = withNextra()
